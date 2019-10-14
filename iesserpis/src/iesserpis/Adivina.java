b36ca4f21c0fe59a4b896075887ad074cf40ccbd
Anajose231296 <anarod@alu.iesserpis.org>
2019-10-14 13:36:15 +0200

    Añadido ADivina mostrando whilw y for

---
+++ iesserpis/src/iesserpis/Adivina.java
@@ -0,0 +1,21 @@
+package iesserpis;
+import java.util.Scanner;
+
+public class Adivina {
+
+	public static void main(String[] args) {
+		int contador = 1;
+		
+		while ( contador <= 5) {
+			System.out.println("contador = " +contador);
+			  contador = contador + 1;
+			  
+		}
+		
+
+		//for (int contadores = 1; contadores <= 5; contadores ++)
+			//System.out.println("contador = " +contador);
+		System.out.println("Fin del programa.");
+	}
+
+}
