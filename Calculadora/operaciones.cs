b96bfaae4d534942c10cfd0e0432644653ea3c04
Anajose231296 <anarod@alu.iesserpis.org>
2020-02-24 13:27:14 +0100

    clase operaciones

---
+++ Calculadora/operaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+public class operaciones
+{
+    public double suma(double valor_a, double valor_b)
+    {
+        return valor_a + valor_b;
+    }
+
+    public double resta(double valor_a, double valor_b)
+    {
+        return valor_a - valor_b;
+    }
+
+    public double multiplicar(double valor_a, double valor_b)
+    {
+        return valor_a * valor_b;
+    }
+
+    public double dividir(double valor_a, double valor_b)
+    {
+        return valor_a / valor_b;
+    }
+}
